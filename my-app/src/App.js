83d3cb2a56c4a89a83c34b38770d02f50cb919f7
FintanMi <fintanjnr@gmail.com>
2023-10-10 15:08:55 +0000

    converted classAPI to functional components with hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,16 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
-import ContentAPI from './components/ContentAPI';
+// import ContentAPI from './components/ContentAPI';
 // import ContentHooks from './components/ContentHooks';
+import ContentAPIHooks from './components/ContentAPIHooks';
 
 function App() {
   return (
     <div className={css.App}>
       <NavBarForm />
       <Sidebar />
-      <ContentAPI />
+      <ContentAPIHooks />
     </div>
   );
 }
